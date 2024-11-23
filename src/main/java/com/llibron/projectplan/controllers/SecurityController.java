9d2ec4f3b546556bc2f0bce6d4e998d9fe7b5af0
Lorenzo Libron <llibron@exist.com>
2024-11-24 06:28:10 +0800

    set cookies

--- src/main/java/com/llibron/projectplan/controllers/SecurityController.java
+++ src/main/java/com/llibron/projectplan/controllers/SecurityController.java
@@ -2,6 +2,7 @@ package com.llibron.projectplan.controllers;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request, HttpServletResponse response) {
         Cookie[] cookies = request.getCookies();
 
         for(Cookie cookie : cookies) {
             System.out.println(cookie.getName() + " " + cookie.getValue());
         }
+
+        Cookie testCookie = new Cookie("test", "test");
+        testCookie.setPath("/");
+        testCookie.setHttpOnly(true);
+        testCookie.setSecure(true);
+
+        response.addCookie(testCookie);
 
         return "Hello " + request.getSession().getId();
     }
